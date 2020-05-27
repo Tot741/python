0d9fc7ed54eeb2425ae503b59872a42de87739b2
Oleg Upovalov <2987026@mail.ru>
2020-05-27 17:10:55 +0300

    Задачи 1-7 5-го урока. reformat code. исправил среднюю прибыль

--- task_7.py
+++ task_7.py
@@ -2,14 +2,16 @@ import re, json
 
 try:
     average_profit = 0
+    i = 0
     dict_firm = {}
     with open('text_7.txt') as file_obj:
         for line in file_obj:
             profit = int(re.findall(r'\d+', line)[0]) - int(re.findall(r'\d+', line)[1])
             name = line.split()[0]
             average_profit += profit if profit > 0 else profit
+            i += 1 if profit > 0 else i
             dict_firm.update({name: profit})
-    dict_average_profit = {'average_profit': average_profit}
+    dict_average_profit = {'average_profit': average_profit / i}
     finish_list = [dict_firm, dict_average_profit]
     with open('text_7.json', 'w', encoding='utf-8') as file_obj:
         json.dump(finish_list, file_obj)
